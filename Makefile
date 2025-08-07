32fd2b223c43710ffed9f10fc2f092873c63f7a2
anderslatif <anderslatif@users.noreply.github.com>
2025-08-07 17:13:53 +0200

    Fixed the run all experiments file and added a make command for it

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: init_db
+.PHONY: init_db migrate_db run_all_experiments
 
 init_db:
 	@rm -f database.db
@@ -8,4 +8,6 @@ init_db:
 migrate_db:
 	@alembic -c database/alembic.ini revision --autogenerate -m "New migration"
 	@alembic -c database/alembic.ini upgrade head
-	
+
+run_all_experiments:
+	@python3 -m meta.run_all_experiment_series
--- meta/run_all_experiment_series.py
+++ meta/run_all_experiment_series.py
@@ -1,33 +1,34 @@
-from database.queries.experiment_series_queries import select_all_experiment_series, update_experiment_series
-from database.queries.experiments_queries import delete_experiments_by_series_name
-from database.session import SessionLocal
-from experiments import run_experiments
+if __name__ == "__main__":
+    from database.queries.experiment_series_queries import select_all_experiment_series, update_experiment_series
+    from database.queries.experiments_queries import delete_experiments_by_series_name
+    from database.session import SessionLocal
+    from experiments import run_experiments
 
 
-###############################################
-# Config
+    ###############################################
+    # Config
 
-experiments_not_to_run = [
-    "_default"
-]
-
-
-###############################################
+    experiments_not_to_run = [
+        "_default"
+    ]
 
 
-session = SessionLocal()
+    ###############################################
 
-all_experiment_series = select_all_experiment_series(session)
 
-for experiment_series in all_experiment_series:
-    if experiment_series.experiment_series_name in experiments_not_to_run:
-        print("Skipping experiment series:", experiment_series.experiment_series_name)
-        continue
+    session = SessionLocal()
 
-    experiment_series_name = experiment_series.experiment_series_name
-    print(experiment_series_name)
+    all_experiment_series = select_all_experiment_series(session)
 
-    delete_experiments_by_series_name(session, experiment_series_name) 
-    update_experiment_series(session, experiment_series_name, { "is_experiments_outdated": False })
-    run_experiments(experiment_series)
-    print("Running experiments for series:", experiment_series_name)
+    for experiment_series in all_experiment_series:
+        if experiment_series.experiment_series_name in experiments_not_to_run:
+            print("Skipping experiment series:", experiment_series.experiment_series_name)
+            continue
+
+        experiment_series_name = experiment_series.experiment_series_name
+        print(experiment_series_name)
+
+        delete_experiments_by_series_name(session, experiment_series_name) 
+        update_experiment_series(session, experiment_series_name, { "is_experiments_outdated": False })
+        run_experiments(experiment_series)
+        print("Running experiments for series:", experiment_series_name)
