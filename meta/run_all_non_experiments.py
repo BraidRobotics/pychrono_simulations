460b6beb90010a52552cab21c5e37749f2e37689
anderslatif <anderslatif@users.noreply.github.com>
2025-11-09 19:03:01 +0100

    Created a 'run all experiment series non experiments' script

---
+++ meta/run_all_non_experiments.py
@@ -0,0 +1,12 @@
+from experiments import run_non_experiment
+from database.queries.experiment_series_queries import select_all_experiment_series
+from database.session import SessionLocal
+
+if __name__ == '__main__':
+    session = SessionLocal()
+    all_experiment_series = select_all_experiment_series(session)
+
+    for experiment_series in all_experiment_series:
+        run_non_experiment(experiment_series, will_visualize=False)
+
+    session.close()
